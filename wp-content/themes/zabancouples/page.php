cbc40800a1c98f4884d3c3039566c1a87ad9d4ad
Nicholas Tennies <nick@brainbytescreative.com>
2015-11-30 14:52:48 -0500

    simplifies page to remove dynamic content template. we are using visual composer instead

--- wp-content/themes/zabancouples/page.php
+++ wp-content/themes/zabancouples/page.php
@@ -13,9 +13,9 @@ get_header(); ?>
     <main id="main" role="main">
         <?php while (have_posts()) { ?>
             <?php the_post(); ?>
-            <?php get_template_part( 'content', 'page' ); ?>
+            <?php the_content(); ?>
         <?php } ?>
     </main>
-</div>    
+</div>
 
 <?php get_footer(); ?>
